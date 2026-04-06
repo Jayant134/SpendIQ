d1aa4c5c7a9c8021d960176ad03b62b6d4e9efd2
Jayant134 <masterjt456@gmail.com>
2026-04-06 19:11:54 +0530

    Decoration in home screen

--- app/src/main/java/com/example/spendiq/HomeScreen.kt
+++ app/src/main/java/com/example/spendiq/HomeScreen.kt
@@ -1,12 +1,15 @@
 package com.example.spendiq
 
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.aspectRatio
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
@@ -18,11 +21,13 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.dimensionResource
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import com.example.spendiq.ui.theme.DisplayXLargeBold
 
 @Composable
 fun HomeScreen(
@@ -36,22 +41,28 @@ fun HomeScreen(
         LazyColumn(
             modifier = Modifier
                 .fillMaxSize()
-                .padding(top = dimensionResource(R.dimen.padding_medium),),
+                .padding(all = dimensionResource(R.dimen.padding_large)),
             userScrollEnabled = true,
         ) {
             item {
                 Card(      //daily budget
+                    shape = MaterialTheme.shapes.medium,
                     modifier = Modifier
-                        .fillMaxWidth()
-                        .padding(dimensionResource(R.dimen.padding_small)),
-                    elevation = CardDefaults.cardElevation(2.dp)
+                        .height(dimensionResource(R.dimen.otherCardHeight))
+                        .fillMaxWidth(),
+ //                       .padding(start = dimensionResource(R.dimen.padding_large), end = dimensionResource(R.dimen.padding_large)),
+                    elevation = CardDefaults.cardElevation(3.dp)
                 ) {
-                    Row(
-                        verticalAlignment = Alignment.CenterVertically
+                    Column(
+                        modifier = Modifier
+                            .fillMaxSize()
+                            .padding(dimensionResource(R.dimen.padding_medium)),
+                        horizontalAlignment = Alignment.Start,
+                        verticalArrangement = Arrangement.Center
                     ) {
                         Text(
                             text = stringResource(R.string.daily_budget),
-                            style = MaterialTheme.typography.bodyLarge
+                            style = MaterialTheme.typography.bodyMedium
                         )
                         Text(
                             text = stringResource(R.string.val_daily_budget),
@@ -60,54 +71,131 @@ fun HomeScreen(
                     }
                 }
 
+                Spacer(modifier = Modifier.height(dimensionResource(R.dimen.spacerCardHeight)))
+
                 Card(  //daily expense
+                    shape = MaterialTheme.shapes.medium,    //for rounded corner
                     modifier = Modifier
-                        .fillMaxWidth()
-                        .padding(dimensionResource(R.dimen.padding_small)),
-                    elevation = CardDefaults.cardElevation(2.dp)
+                        .height(dimensionResource(R.dimen.bigCardHeight))
+                        .fillMaxWidth(),
+//                        .padding(start = dimensionResource(R.dimen.padding_large), end = dimensionResource(R.dimen.padding_large)),
+                    elevation = CardDefaults.cardElevation(3.dp)
                 ) {
-                    Row() {
+                    Column(
+                        modifier = Modifier
+                            .fillMaxSize()
+                            .padding(dimensionResource(R.dimen.padding_medium)),
+                        horizontalAlignment = Alignment.Start,
+                        verticalArrangement = Arrangement.Center
+                    ) {
                         Text(
-                            text = stringResource(R.string.dailyExpense)
+                            text = stringResource(R.string.dailyExpense),
+                            style = MaterialTheme.typography.displayLarge
                         )
                         Text(
-                            text = stringResource(R.string.valDailyExpense)
+                            text = stringResource(R.string.valDailyExpense),
+                            style = DisplayXLargeBold
                         )
                     }
                 }
+
+                Spacer(modifier = Modifier.height(dimensionResource(R.dimen.spacerCardHeight)))
 
                 Card(  //monthly expense
+                    shape = MaterialTheme.shapes.medium,
                     modifier = Modifier
-                        .fillMaxWidth()
-                        .padding(dimensionResource(R.dimen.padding_small)),
-                    elevation = CardDefaults.cardElevation(2.dp)
+                        .height(dimensionResource(R.dimen.otherCardHeight))
+                        .fillMaxWidth(),
+//                        .padding(start = dimensionResource(R.dimen.padding_large), end = dimensionResource(R.dimen.padding_large)),
+                    elevation = CardDefaults.cardElevation(3.dp)
                 ) {
-                    Row() {
+                    Column(
+                        modifier = Modifier
+                            .fillMaxSize()
+                            .padding(dimensionResource(R.dimen.padding_large)),
+                        horizontalAlignment = Alignment.Start,
+                        verticalArrangement = Arrangement.Center
+                    ) {
                         Text(
-                            text = stringResource(R.string.monthlyExpense)
+                            text = stringResource(R.string.monthlyExpense),
+                            style = MaterialTheme.typography.bodyMedium
                         )
                         Text(
-                            text = stringResource(R.string.valMonthlyExpense)
+                            text = stringResource(R.string.valMonthlyExpense),
+                            style = MaterialTheme.typography.titleSmall
                         )
                     }
                 }
 
-                Column(
+                Spacer(modifier = Modifier.height(dimensionResource(R.dimen.spacerCardHeight)))
+
+                Card(  //AI insight
+                    shape = MaterialTheme.shapes.medium,
                     modifier = Modifier
-                        .fillMaxWidth()
+                        .fillMaxWidth(),
+//                        .padding(start = dimensionResource(R.dimen.padding_large), end = dimensionResource(R.dimen.padding_large)),
+                    elevation = CardDefaults.cardElevation(3.dp)
                 ) {
-                    Text(
-                        text = stringResource(R.string.aiInsight)
-                    )
-                    Text(
-                        text = stringResource(R.string.food)
-                    )
-                    Text(
-                        text = stringResource(R.string.travel)
-                    )
+                    Column(
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .padding(dimensionResource(R.dimen.padding_large)),
+                        verticalArrangement = Arrangement.Center
+                    ) {
+                        Text(
+                            text = stringResource(R.string.aiInsight)
+                        )
+                        Text(
+                            text = stringResource(R.string.food)
+                        )
+                        Text(
+                            text = stringResource(R.string.travel)
+                        )
+                    }
                 }
+            }//item-1 end
 
+            //item-2: Image Cards
+            item{
+                Text(
+                    text = "Analysis",
+                    style = MaterialTheme.typography.titleSmall,
+                    modifier = Modifier.padding(top =8.dp, bottom = 8.dp)
+                )
 
+                Row(
+                    modifier = Modifier.fillMaxWidth(),
+                    horizontalArrangement = Arrangement.spacedBy(12.dp)
+                ) {
+
+                    Card(
+                        modifier = Modifier
+                            .weight(1f)
+                            .aspectRatio(1f),
+                        elevation = CardDefaults.cardElevation(3.dp)
+                    ) {
+                        Image(
+                            painter = painterResource(R.drawable.piechart),
+                            contentDescription = null,
+                            contentScale = ContentScale.Crop,
+                            modifier = Modifier.fillMaxSize()
+                        )
+                    }
+
+                    Card(
+                        modifier = Modifier
+                            .weight(1f)
+                            .aspectRatio(1f),
+                        elevation = CardDefaults.cardElevation(3.dp)
+                    ) {
+                        Image(
+                            painter = painterResource(R.drawable.recommededchart),
+                            contentDescription = null,
+                            contentScale = ContentScale.Crop,
+                            modifier = Modifier.fillMaxSize()
+                        )
+                    }
+                }
             }
         }
         //buttons
@@ -115,6 +203,8 @@ fun HomeScreen(
             modifier = Modifier
                 .fillMaxWidth()
                 .align(Alignment.BottomCenter)
+                .padding(dimensionResource(R.dimen.padding_medium)),
+            horizontalArrangement = Arrangement.SpaceEvenly
         ) {
             Button(
                 onClick = onAddClick
--- app/src/main/java/com/example/spendiq/ui/theme/Type.kt
+++ app/src/main/java/com/example/spendiq/ui/theme/Type.kt
@@ -4,10 +4,10 @@ import androidx.compose.material3.Typography
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
-import androidx.compose.ui.text.font.FontStyle
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
 import com.example.spendiq.R
+
 
 val Lexend = FontFamily(
     Font(R.font.lexend_light),
@@ -18,21 +18,27 @@ val Typography = Typography(
     displayLarge = TextStyle(
         fontFamily = Lexend,
         fontWeight = FontWeight.Normal,
-        fontSize = 30.sp
+        fontSize = 14.sp
     ),
     titleSmall = TextStyle(
         fontFamily = Lexend,
-        fontWeight = FontWeight.Bold,
+        fontWeight = FontWeight.Normal,
         fontSize = 20.sp
     ),
-    bodyLarge = TextStyle(
-        fontFamily = Lexend,
-        fontWeight = FontWeight.Normal,
-        fontSize = 16.sp
-    ),
+//    bodyLarge = TextStyle(
+//        fontFamily = Lexend,
+//        fontWeight = FontWeight.Normal,
+//        fontSize = 22.sp
+//    ),
     bodyMedium = TextStyle(
         fontFamily = Lexend,
         fontWeight = FontWeight.Light,
-        fontSize = 14.sp
+        fontSize = 12.sp
     )
+)
+
+val DisplayXLargeBold = TextStyle(
+    fontFamily = Lexend,
+    fontWeight = FontWeight.Bold,
+    fontSize = 32.sp
 )
